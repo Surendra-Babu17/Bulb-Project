729371c1c8ec67c7d696528ac28108fb77448f96
SURENDRA BABU <surendrasvsk@gmail.com>
2025-09-04 15:04:46 +0530

    protofolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    document.getElementById("image").src="./New folder/image.png";
+    
+}
+
+function bulbOff(){
+    document.getElementById("image").src="./New folder/image copy.png";
+    
+}
